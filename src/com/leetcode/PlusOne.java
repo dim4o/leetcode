5565f4882ede174bed5c47a5e9a9f1bd8f99954f
dimcho <dimcho.nedev+git@gmail.com>
2020-02-05 23:28:52 +0200

    Add "Plus One" problem solution
    See: https://leetcode.com/problems/plus-one/

---
+++ src/com/leetcode/PlusOne.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        if (digits[digits.length - 1] < 9) {
+            digits[digits.length - 1]++;
+            return digits;
+        }
+        int reminder = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            
+            int curr_val = digits[i] + reminder;
+            if (curr_val == 10) {
+                reminder = 1;
+                digits[i] = 0;
+            } else {
+                digits[i] = curr_val;
+                reminder = 0;
+            }
+        }
+        if (reminder == 1) {
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+        }
+        return digits;
+    }
+
+    public static void main(String... args) {
+        PlusOne sln = new PlusOne();
+        System.out.println(Arrays.toString(sln.plusOne(new int[] {1, 2, 3})));
+        System.out.println(Arrays.toString(sln.plusOne(new int[] {1, 2, 8})));
+        System.out.println(Arrays.toString(sln.plusOne(new int[] {1, 2, 9})));
+        System.out.println(Arrays.toString(sln.plusOne(new int[] {1, 9, 9})));
+        System.out.println(Arrays.toString(sln.plusOne(new int[] {9, 9, 9})));
+
+    }
+}
