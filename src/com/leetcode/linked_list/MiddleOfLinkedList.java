a61f55125efbede27a9bf128e84ec9908f38b6e1
dimcho <dimcho.nedev+git@gmail.com>
2020-02-07 22:54:41 +0200

    Add "Middle Of The Linked List" problem solution
    See: https://leetcode.com/problems/middle-of-the-linked-list/

---
+++ src/com/leetcode/linked_list/MiddleOfLinkedList.java
@@ -0,0 +1,35 @@
+// Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+// If there are two middle nodes, return the second middle node.
+// See: https://leetcode.com/problems/middle-of-the-linked-list/
+
+package com.leetcode.linked_list;
+
+public class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        MiddleOfLinkedList sln = new MiddleOfLinkedList();
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        ListNode n6 = new ListNode(6);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+        System.out.println(sln.middleNode(n1).val);
+    }
+}
