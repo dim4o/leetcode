fd6ffb647b3c57ffa42817871ee932c4f216fe36
dimcho <dimcho.nedev+git@gmail.com>
2020-02-16 10:47:58 +0200

    Add solution for "Maximum Depth Of Binary Tree" problem
    See: https://leetcode.com/problems/maximum-depth-of-binary-tree/

---
+++ src/com/leetcode/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,45 @@
+// Maximum Depth of Binary Tree
+// See: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+package com.leetcode.tree;
+
+import static com.leetcode.tree.util.BinTreeUtil.*;
+
+import com.leetcode.tree.util.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode root, int depth) {
+        if (root == null) {
+            return depth;
+        }
+        
+        return Math.max(dfs(root.left, depth + 1), dfs(root.right, depth + 1));
+    }
+    
+    public static void main(String[] args) {
+        MaximumDepthOfBinaryTree sln = new MaximumDepthOfBinaryTree();
+
+        TreeNode t0 = initTree();
+        System.out.println(sln.maxDepth(t0)); // 0
+        
+        TreeNode t1 = initTree(1);
+        System.out.println(sln.maxDepth(t1)); // 1
+        
+        TreeNode t2 = initTree(1, 1);
+        System.out.println(sln.maxDepth(t2)); // 2
+        
+        TreeNode t3 = initTree(1, 2, null, 3);
+        System.out.println(sln.maxDepth(t3)); // 3
+        
+        TreeNode t4 = initTree(1, 2, 4, 3, null);
+        System.out.println(sln.maxDepth(t4)); // 3
+        
+        TreeNode t5 = initTree(3, 9, 20, null, null, 15, 7);
+        System.out.println(sln.maxDepth(t5));
+    }
+}
