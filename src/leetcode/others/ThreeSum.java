11a6b14bec9fc8599f9c0105e5101972f63feee0
dimcho <dimcho.nedev+git@gmail.com>
2020-03-08 01:40:11 +0200

    Update "3Sum" solution
    See: https://github.com/dim4o/leetcode/commits/master

--- src/leetcode/others/ThreeSum.java
+++ src/leetcode/others/ThreeSum.java
@@ -13,8 +13,39 @@ import java.util.List;
 import java.util.Set;
 
 public class ThreeSum {
+    // Two pointers solution (accepted)
+    // Moreover the same O(n^2) time complexity this solution is faster than the others,
+    // because no need of Set
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> triples = new ArrayList<>();
+
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    triples.add(Arrays.asList(new Integer[] { nums[i], nums[left], nums[right] }));
+                    while (left < right && nums[left] == nums[left+1]) left++;
+                    while (left < right && nums[right] == nums[right-1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return triples;
+    }
+
     // Sort and memorize (accepted)
-    public List<List<Integer>> threeSum1(int[] nums) {
+    public List<List<Integer>> threeSum2(int[] nums) {
         Arrays.sort(nums);
         Set<List<Integer>> triples = new HashSet<>();
 
@@ -28,15 +59,16 @@ public class ThreeSum {
                     List<Integer> triple = Arrays.asList(new Integer[] { nums[i], third, nums[j] });
                     if (!triples.contains(triple))
                         triples.add(triple);
-                } else s.add(third);
+                } else
+                    s.add(third);
             }
         }
-        
+
         return new ArrayList<List<Integer>>(triples);
     }
-    
+
     // Without sort (accepted)
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> threeSum1(int[] nums) {
         Set<Integer> used = new HashSet<>();
         Set<List<Integer>> triples = new HashSet<>();
 
@@ -46,15 +78,17 @@ public class ThreeSum {
                 if (!used.contains(nums[j])) {
                     int third = -nums[i] - nums[j];
                     if (s.contains(nums[j])) {
-                        List<Integer> triple = Arrays.asList(new Integer[] { nums[i], third, nums[j] });
+                        List<Integer> triple = Arrays
+                                .asList(new Integer[] { nums[i], third, nums[j] });
                         if (!triples.contains(triple))
                             triples.add(triple);
-                    } else s.add(third);
+                    } else
+                        s.add(third);
                 }
             }
             used.add(nums[i]);
         }
-        
+
         return new ArrayList<List<Integer>>(triples);
     }
 
