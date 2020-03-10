22a5324854a336fbd2a7b63a90c6503b0e3cfbe2
dimcho <dimcho.nedev+git@gmail.com>
2020-03-10 12:50:40 +0200

    Add solution for "Longest Continuous Increasing Subsequence" problem
    See: https://leetcode.com/problems/longest-continuous-increasing-subsequence/

---
+++ src/leetcode/others/LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,28 @@
+// Given an unsorted array of integers, find the length of 
+// longest continuous increasing subsequence (subarray).
+// See: https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+
+package leetcode.others;
+
+public class LongestContinuousIncreasingSubsequence {
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        int lcis = 1, count = 1;
+        for (int i = 1; i < nums.length; i++)
+            if (nums[i] > nums[i - 1])
+                lcis = Math.max(lcis, ++count);
+            else
+                count = 1;
+        
+        return lcis;
+    }
+
+    public static void main(String[] args) {
+        LongestContinuousIncreasingSubsequence sln = new LongestContinuousIncreasingSubsequence();
+        System.out.println(sln.findLengthOfLCIS(new int[] {}));
+        System.out.println(sln.findLengthOfLCIS(new int[] { 1, 3, 5, 4, 7 }));
+        System.out.println(sln.findLengthOfLCIS(new int[] { 2, 2, 2, 2, 2 }));
+        System.out.println(sln.findLengthOfLCIS(new int[] { 1, 1, 2, 3, 1 }));
+    }
+
+}
