5ef4312ae03581047e813115ac363b792b9631cb
dimcho <dimcho.nedev+git@gmail.com>
2020-03-21 20:34:34 +0200

    Update "Reverse Linked List" solution
    Add recursive solutions
    
    See: https://leetcode.com/problems/reverse-linked-list/
    See: https://leetcode.com/problems/reverse-linked-list/discuss/546673/Java-Another-Recursive-Approach

--- src/leetcode/linkedlist/ReverseLinkedList.java
+++ src/leetcode/linkedlist/ReverseLinkedList.java
@@ -1,7 +1,7 @@
 // Reverse a singly linked list.
+// A linked list can be reversed either iteratively or recursively. Could you implement both?
 // See: https://leetcode.com/problems/reverse-linked-list/
-
-// TODO: A linked list can be reversed either iteratively or recursively. Could you implement both?
+// My solution at leetcode: https://leetcode.com/problems/reverse-linked-list/discuss/546673/Java-Another-Recursive-Approach
 
 package leetcode.linkedlist;
 
@@ -11,7 +11,42 @@ import static leetcode.util.linkedlist.LinkedListUtil.printLinkedList;
 import leetcode.util.linkedlist.ListNode;
 
 public class ReverseLinkedList {
+
+    /**
+     * Recursive solution.
+     * Great explanation here: https://www.youtube.com/watch?time_continue=4&v=MRe3UsRadKw&feature=emb_logo
+     */
     public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;    
+        
+        ListNode curr = reverseList(head.next);
+
+        head.next.next = head;;
+        head.next = null;
+
+        return curr;
+    }
+
+    /**
+     * Recursive solution.
+     * See: https://leetcode.com/problems/reverse-linked-list/discuss/546673/Java-Another-Recursive-Approach
+     */
+    public ListNode reverseList_var3(ListNode head) {
+        return reverse(null, head);
+    }
+
+    private ListNode reverse(ListNode reversedpart, ListNode curr) {
+        if (curr == null)
+            return reversedpart;
+
+        ListNode nextNode = curr.next;
+        curr.next = reversedpart;
+
+        return reverse(curr, nextNode);
+    }
+
+    public ListNode reverseList_var2(ListNode head) {
         ListNode prevNode = null;
         ListNode currNode = head;
         while (currNode != null) {
@@ -20,10 +55,10 @@ public class ReverseLinkedList {
             prevNode = currNode;
             currNode = nextNode;
         }
-        
+
         return prevNode;
     }
-    
+
     public ListNode reverseList_var1(ListNode head) {
         if (head == null) {
             return null;
@@ -31,22 +66,22 @@ public class ReverseLinkedList {
         ListNode prevNode = null;
         ListNode currNode = head;
         ListNode nextNode = head.next;
-        
+
         while (nextNode != null) {
             currNode.next = prevNode;
             prevNode = currNode;
             currNode = nextNode;
-            nextNode = nextNode.next;            
+            nextNode = nextNode.next;
         }
-        
+
         currNode.next = prevNode;
-        
+
         return currNode;
     }
 
     public static void main(String... args) {
         ReverseLinkedList sln = new ReverseLinkedList();
-        
+
         ListNode n = initList(1, 2, 3, 4);
         ListNode reversed = sln.reverseList(n);
         printLinkedList(reversed);
