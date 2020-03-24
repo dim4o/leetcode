87bdce60ebed827f50750e4684bfc97dac0f49e7
dimcho <dimcho.nedev+git@gmail.com>
2020-03-24 17:37:34 +0200

    Update solution for "Sort an Array" problem
    Add Merge Sort algorithm
    
    See: https://leetcode.com/problems/sort-an-array/

--- src/leetcode/others/SortAnArray.java
+++ src/leetcode/others/SortAnArray.java
@@ -4,12 +4,55 @@ import java.util.Arrays;
 
 public class SortAnArray {
     /**
-     * Bubble Sort, not accepted (Time Limit Exceeded).
-     * Moreover the same time complexity as Insertion Sort or Selection Sort
-     * this algorithm seems slower in practice.
-     * O(n^2) time, O(1) space
+     * Merge Sort. n.log(n) time for worst, average and base case. O(n) space, but
+     * can be done and with O(1) space. Stable: Yes
      */
     public int[] sortArray(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int[] mergeSort(int[] nums, int lo, int hi) {
+        int mid = (lo + hi) / 2;
+        if (lo == hi)
+            return nums;
+
+        nums = mergeSort(nums, lo, mid);
+        nums = mergeSort(nums, mid + 1, hi);
+
+        return merge(nums, lo, mid, hi);
+    }
+
+    /**
+     * This is the tricky part of the algorithm.
+     */
+    private int[] merge(int[] nums, int lo, int mid, int hi) {
+        int[] tmp = new int[hi - lo + 1];
+        int l = lo, r = mid + 1;
+        int i = 0;
+        while (l <= mid && r <= hi)
+            if (nums[l] < nums[r])
+                tmp[i++] = nums[l++];
+            else
+                tmp[i++] = nums[r++];
+        // copy the rest of the left part
+        for (int j = l; j <= mid; j++)
+            tmp[i++] = nums[j];
+        // copy the rest of the right part
+        for (int j = r; j <= hi; j++)
+            tmp[i++] = nums[j];
+        // copy the merged part to the original array
+        for (int j = lo; j <= hi; j++)
+            nums[j] = tmp[j - lo];
+
+        return nums;
+    }
+
+    /**
+     * Bubble Sort, not accepted (Time Limit Exceeded). Moreover the same time
+     * complexity as Insertion Sort or Selection Sort this algorithm seems slower in
+     * practice. O(n^2) time, O(1) space
+     */
+    public int[] sortArray_var4(int[] nums) {
         for (int i = nums.length - 1; i >= 1; i--) {
             boolean sorted = true;
             for (int j = 0; j < i; j++) {
@@ -20,12 +63,13 @@ public class SortAnArray {
                     nums[j + 1] = tmp;
                 }
             }
-            if (sorted) break;
+            if (sorted)
+                break;
         }
 
         return nums;
     }
-    
+
     /**
      * Binary Insertion Sort (Insertion Sort using Binary Search). O(n^2) time for
      * the worst case(reversed order), but the number of comparisons are O(n.log(n))
@@ -58,11 +102,11 @@ public class SortAnArray {
             return r;
 
         int mid = (l + r - 1) / 2;
-        if (arr[mid] == target) 
+        if (arr[mid] == target)
             return mid;
-        else if (target < arr[mid]) 
+        else if (target < arr[mid])
             return binarySearch(arr, l, mid, target);
-        else 
+        else
             return binarySearch(arr, mid + 1, r, target);
     }
 
@@ -105,12 +149,18 @@ public class SortAnArray {
 
     public static void main(String[] args) {
         SortAnArray sln = new SortAnArray();
+
         int[] res = sln.sortArray(new int[] { 2, 4, 3, 1, 7, 5, 6, 1 });
         System.out.println(Arrays.toString(res));
 
-//        System.out.println(Arrays.binarySearch(new int[] {2, 3, 5, 5, 7, 9, 10, 18, 22}, 5));
-        System.out.println(sln.binarySearch(new int[] { 2, 3, 5, 5, 7, 9, 10, 18, 22 }, 0, 9, 6));
+        int[] arr1 = new int[] { 1, 5, 6, 7, 2, 3, 4, 15, 16, 17 };
 
+        System.out.println(Arrays.toString(sln.merge(arr1, 0, 3, 6)));
+
+        int[] arr2 = new int[] { 1, 2, 3, 6, 8, 9, 10, 5, 5, 7, 20, 30, 40 };
+
+        System.out.println(Arrays.toString(sln.merge(arr2, 3, 6, 9)));
+        System.out.println(Arrays.toString(sln.merge(arr2, 0, 1, 2)));
     }
 
 }
