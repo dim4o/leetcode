43fe7d3038a65fb3c67349a491dac24886f42261
dimcho <dimcho.nedev+git@gmail.com>
2020-03-25 17:28:49 +0200

    Update solution for "Sort an Array" problem
    Add Heap Sort algorithm
    
    See: https://leetcode.com/problems/sort-an-array/

--- src/leetcode/others/SortAnArray.java
+++ src/leetcode/others/SortAnArray.java
@@ -3,13 +3,55 @@ package leetcode.others;
 import java.util.Arrays;
 
 public class SortAnArray {
+    /**
+     * Heap Sort.
+     * O(n.log(n)) time.
+     * O(1) space.
+     * Stable: No but can be made stable.
+     */
+    public int[] sortArray(int[] arr) {
+        // 1. First heapify the hole tree: every parent is grater than it's children
+        for (int i = 2 * arr.length - 1; i >= 0; i--)
+            heapify(arr, i, arr.length);
+        // When the Max Heap is created he max value is the at the beginning of the array.
+        // 2. Swap the first and the last element and decrease the heap size with 1.
+        // 3. Call heapify to the reduced heap.
+        for (int i = arr.length - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, 0, i);
+        }
+        
+        return arr;
+    }
+    
+    private void heapify(int[] arr, int start, int end) {
+        int greater = start;
+        int l = start * 2 + 1;
+        int r = start * 2 + 2;
+        if (l < end && r < end && arr[r] > arr[l] && arr[start] < arr[r])
+            greater = r;
+        else if (l < end && arr[start] < arr[l])
+            greater = l;
+        
+        if (start != greater) {
+            swap(arr, start, greater);
+            heapify(arr, greater, end);
+        }
+    }
+    
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    
     /*
      * Quick Sort. 
      * n.log(n) time for average and base case, O(n^2) for the worst case.
      * O(1) space
      * Stable: No
      */
-    public int[] sortArray(int[] nums) {
+    public int[] sortArray_var6(int[] nums) {
         quickSort(nums, 0, nums.length - 1);
         return nums;
     }
@@ -222,6 +264,12 @@ public class SortAnArray {
 
     public static void main(String[] args) {
         SortAnArray sln = new SortAnArray();
+        
+//        int[] arr = new int[] {-4, 0, 7, 4, 9, -5, -1, 0, -7, -1};
+//        int[] arr = new int[] {4, 10, 3, 5, 1};
+//        int[] arr = new int[] {5, 2, 3, 1};
+//        sln.sortArray(arr);
+//        System.out.println(Arrays.toString(arr));
 
         int[] res = sln.sortArray(new int[] { 2, 4, 3, 1, 7, 5, 6, 1 });
         System.out.println(Arrays.toString(res));
