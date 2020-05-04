43c4937d47763cb7945831d7c8219d4c09400ab1
dimcho <dimcho.nedev+git@gmail.com>
2020-05-04 21:24:39 +0300

    Add a solution for "Number Complement" problem (May LeetCoding Challenge)
    See: https://leetcode.com/problems/number-complement/
    See: https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3319/
    See: https://leetcode.com/problems/number-complement/discuss/612426/Java-1-Liner-Solution

---
+++ src/leetcode/challenge/NumberComplement.java
@@ -0,0 +1,23 @@
+// Given a positive integer, output its complement number. The complement strategy is to flip the bits 
+// of its binary representation.
+// See: https://leetcode.com/problems/number-complement/
+// See: https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3319/
+// See: https://leetcode.com/problems/number-complement/discuss/612426/Java-1-Liner-Solution
+
+package leetcode.challenge;
+
+public class NumberComplement {
+    public int findComplement(int num) {
+        /* 
+         * The number of the bits in the binary representation:
+         * int count = (int)(Math.log(num) /  Math.log(2) + 1); 
+         */
+        return num != 0 ? ((~0) << (int)(Math.log(num) /  Math.log(2) + 1)) ^ ~num : 1;
+    }
+    
+    public static void main(String[] args) {
+        NumberComplement sln = new NumberComplement();
+        System.out.println(sln.findComplement(0));
+    }
+
+}
