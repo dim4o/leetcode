cc90b56a6a5c9a0779a17e602075d82f97ec12de
dim4o <dimcho.nedev+git@gmail.com>
2020-05-24 17:37:39 +0300

    Update a solution for "Single Number" problem (challenge)
    See: https://leetcode.com/problems/single-number/
    See: https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3283/

--- src/leetcode/leetcode_challenge/SingleNumber.java
+++ src/leetcode/leetcode_challenge/SingleNumber.java
@@ -1,16 +1,50 @@
 // Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 // Note: Your algorithm should have a linear runtime complexity. 
 // Could you implement it without using extra memory?
+// See: https://leetcode.com/problems/single-number/
 // See: https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3283/
 
 package leetcode.leetcode_challenge;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class SingleNumber {
-    // TODO: Find no extra space linear solution
+    /**
+     * Solution 3 - Simple Bit manipulations.
+     * May be the best solution: O(n) time, O(1) space.
+     */
     public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums)
+            res ^= num;
+        return res;
+    }
+
+    /**
+     * Solution 2 - Simple Math solution.
+     */
+    public int singleNumber_var2(int[] nums) {
+        int sum = 0;
+        int uniqueSum = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            sum += num;
+            if (!set.contains(num)) {
+                set.add(num);
+                uniqueSum += num;
+            }
+        }
+
+        return uniqueSum * 2 - sum;
+    }
+
+    /**
+     * Solution 1 - Hash Table solution.
+     */
+    public int singleNumber_var1(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++)
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
