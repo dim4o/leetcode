e2240e5023fa86409150378b6d62a453e1297e18
dim4o <dimcho.nedev+git@gmail.com>
2020-05-25 16:43:32 +0300

    Add a solution for "Island Perimeter" problem
    See: https://leetcode.com/problems/island-perimeter/

---
+++ src/leetcode/array_and_hashtable/IslandPerimeter.java
@@ -0,0 +1,44 @@
+// You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
+// Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, 
+// and there is exactly one island (i.e., one or more connected land cells).
+// The island doesn't have "lakes" (water inside that isn't connected to the water around the island). 
+// One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. 
+// Determine the perimeter of the island.
+// See: https://leetcode.com/problems/island-perimeter/
+
+package leetcode.array_and_hashtable;
+
+public class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) continue;
+                int curr = 4;
+                if (i > 0 && grid[i - 1][j] == 1)
+                    curr--;
+                if (i < grid.length - 1 && grid[i + 1][j] == 1)
+                    curr--;
+                if (j > 0 && grid[i][j - 1] == 1)
+                    curr--;
+                if (j < grid[0].length - 1 && grid[i][j + 1] == 1)
+                    curr--;
+                res += curr;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        IslandPerimeter sln = new IslandPerimeter();
+        System.out.println(sln.islandPerimeter(new int[][] { 
+            { 0, 1, 0, 0 }, 
+            { 1, 1, 1, 0 }, 
+            { 0, 1, 0, 0 }, 
+            { 1, 1, 0, 0 }
+        }));
+        
+        System.out.println(sln.islandPerimeter(new int[][] {{ 0, 1 }}));
+    }
+
+}
